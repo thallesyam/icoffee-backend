960000f6c309b48c1302842e101f704f7680968c
thallesyam <thallesyam@gmail.com>
2023-03-23 14:02:32 -0300

    adjusting cookies

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import { routes } from './routes'
-import { PrismaClient } from '.prisma/client'
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import { routes } from "./routes"
+import { PrismaClient } from ".prisma/client"
 
 dotenv.config()
 
 export const prisma = new PrismaClient()
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
-app.use(routes);
+app.use(routes)
 
 const port = 5000
 app.listen(port, () => {
-  console.log(`Server started on port: ${port}`);
-});
+  console.log(`Server started on port: ${port}`)
+})
